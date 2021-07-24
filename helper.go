09dcad281a016ad8ff16ab6bc3a57d5fba7e8d11
jamesgawn <james@gawn.uk>
2021-07-24 16:01:25 +0100

    Export public methods

--- helper.go
+++ helper.go
@@ -9,12 +9,12 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/route53/types"
 )
 
-func loadConfig() (aws.Config, error) {
+func LoadConfig() (aws.Config, error) {
 	awsConfig, awsConfigError := config.LoadDefaultConfig(context.TODO())
 	return awsConfig, awsConfigError
 }
 
-func findZone(ctx context.Context, client *route53.Client, zoneName *string) (*types.HostedZone, error) {
+func FindZone(ctx context.Context, client *route53.Client, zoneName *string) (*types.HostedZone, error) {
 
 	listHostedZonesInput := &route53.ListHostedZonesInput{}
 	zoneList, zoneListError := client.ListHostedZones(ctx, listHostedZonesInput)
@@ -32,7 +32,7 @@ func findZone(ctx context.Context, client *route53.Client, zoneName *string) (*t
 	return &types.HostedZone{}, fmt.Errorf("unable to find zone: %s", *zoneName)
 }
 
-func updateRecord(ctx context.Context, client *route53.Client, zone *types.HostedZone, domain *string, ip *string) error {
+func UpdateRecord(ctx context.Context, client *route53.Client, zone *types.HostedZone, domain *string, ip *string) error {
 	params := &route53.ChangeResourceRecordSetsInput{
 		ChangeBatch: &types.ChangeBatch{
 			Changes: []types.Change{
